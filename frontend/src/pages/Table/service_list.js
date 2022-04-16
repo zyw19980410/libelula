1e067e9fb2c5307202c62c597361da1a053c28c0
Michael Lee <notmot@163.com>
2022-04-16 10:52:32 +0800

    add product

--- frontend/src/pages/Table/service_list.js
+++ frontend/src/pages/Table/service_list.js
@@ -1,13 +1,89 @@
-import { Table, Divider, Tag } from 'antd';
+import { Table, Divider, Tag, Input, Form, Button, PageHeader } from 'antd';
+import request from 'umi-request'
+import Auth from '../../store/auth'
 
 class service_list extends React.Component {
+    test = (event) => {
+        event.preventDefault()
+        // console.log(this.state)
+        let self = this
+        request.post('http://localhost:10000/api/admin/product', {
+            data: {
+                "product": {
+                  "product_title": this.state.title,
+                  "on_sale": 1
+                },
+                "categories":null,
+                "images": [
+                  {
+                    "image_id": 2,
+                    "sort": 0
+                  }
+                ],
+                "content": null,
+                "variants": [
+                  {
+                    "variant_title": this.state.title,
+                    "price": parseInt(this.state.price),
+                    "quantity": parseInt(this.state.stock)
+                  }
+                ]
+              },
+            headers: {Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9sb2NhbGhvc3Q6MTAwMDBcL2FwaVwvYWRtaW5cL2xvZ2luIiwiaWF0IjoxNjUwMDc2OTg0LCJleHAiOjE2NTAwODA1ODQsIm5iZiI6MTY1MDA3Njk4NCwianRpIjoiamZnVVFwbE0wdE1oNWdZZyIsInN1YiI6MSwicHJ2IjoiZGY4ODNkYjk3YmQwNWVmOGZmODUwODJkNjg2YzQ1ZTgzMmU1OTNhOSJ9.9469j_sA_YmOUuPjbzwN5qqwAym-SUmUFGEz1DpR_7M`}
+        }).then(function (response) {
+            self.setState({products : [...self.state.products, {
+            id: 4,
+            title: self.state.title,
+            price: self.state.price,
+            stock: self.state.stock,}]})
+        })
+        
+    }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            products: [],
+            title: "",
+            price: 0,
+            stock: 0,
+        }
+
+        let self = this
+        request
+            .get('http://localhost:10000/api/product?page=1&pageSize=100?page=1&pageSize=100')
+            .then(function (response) {
+                let products = []
+                response['body']['data'].forEach((v, i) => {
+                    products.push({
+                        id: v['id'],
+                        title: v['product_title'],
+                        price: v['price'],
+                        stock: v['stock'],
+                    })
+                })
+                self.setState({
+                    products: products
+                })
+            })
+            .catch(function (error) {
+                console.log(error)
+                alert(error.data['message'])
+            })
+
+    }
 
     columns = [
         {
-            title: 'Service',
-            dataIndex: 'service',
-            key: 'service',
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
             render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Product Title',
+            dataIndex: 'title',
+            key: 'product_title',
         },
         {
             title: 'Price',
@@ -15,39 +91,39 @@ class service_list extends React.Component {
             key: 'price',
         },
         {
-            title: 'Material',
-            dataIndex: 'material',
-            key: 'material',
+            title: 'Stock',
+            dataIndex: 'stock',
+            key: 'stock',
         },
-        {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: tags => (
-                <span>
-        {tags.map(tag => {
-            let color = tag.length > 7 ? 'geekblue' : 'green';
-            if (tag === 'handcraft') {
-                color = 'volcano';
-            }
-            return (
-                <Tag color={color} key={tag}>
-                    {tag.toUpperCase()}
-                </Tag>
-            );
-        })}
-      </span>
-            ),
-        },
+        //     {
+        //         title: 'Tags',
+        //         key: 'tags',
+        //         dataIndex: 'tags',
+        //         render: tags => (
+        //             <span>
+        //     {tags.map(tag => {
+        //         let color = tag.length > 7 ? 'geekblue' : 'green';
+        //         if (tag === 'handcraft') {
+        //             color = 'volcano';
+        //         }
+        //         return (
+        //             <Tag color={color} key={tag}>
+        //                 {tag.toUpperCase()}
+        //             </Tag>
+        //         );
+        //     })}
+        //   </span>
+        //         ),
+        //     },
         {
             title: 'Action',
             key: 'action',
             render: (text, record) => (
                 <span>
-        <a>Modify</a>
-        <Divider type="vertical" />
-        <a>Delete</a>
-      </span>
+                    <a>Modify</a>
+                    <Divider type="vertical" />
+                    <a >Delete</a>
+                </span>
             ),
         },
     ];
@@ -78,8 +154,44 @@ class service_list extends React.Component {
     ];
 
     render() {
+
         return (
-            <Table columns={this.columns} dataSource={this.data} />
+            <div>
+                <Table columns={this.columns} dataSource={this.state.products} />
+
+                <Form layout="inline" onSubmit={this.test}>
+                    <Form.Item>
+                        <text>Add New Product: </text>
+                    </Form.Item>
+                    <Form.Item>
+                        <Input
+                            placeholder="Product Title"
+                            value={this.state.title}
+                            onChange={(p) => {this.setState({title : p.target.value})}}
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input
+                            placeholder="Price"
+                            value={this.state.price}
+                            onChange={(p) => {this.setState({price : p.target.value})}}
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Input
+                            placeholder="stock"
+                            type="stock"
+                            value={this.state.stock}
+                            onChange={(p) => {this.setState({stock : p.target.value})}}
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Button type="primary" htmlType="submit">
+                            Commit
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </div>
         )
     }
 }
